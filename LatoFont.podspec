dcdbab89dcd21416f41292954e4b092372944c8b
CJ Zeiger <cjzeiger@gmail.com>
2018-05-15 11:28:18 -0700

    Update for ios10

--- LatoFont.podspec
+++ LatoFont.podspec
@@ -3,12 +3,11 @@ Pod::Spec.new do |s|
   s.name             = 'LatoFont'
   s.version          = '2.0.1'
   s.summary          = 'Brings Lato font to iOS.'
-  s.homepage         = 'https://github.com/michalkonturek/LatoFont'
+  s.homepage         = 'https://github.com/cj-zeiger/LatoFont'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
   s.author           = { 'Michal Konturek' => 'michal.konturek@gmail.com' }
-  s.source           = { :git => 'https://github.com/michalkonturek/LatoFont.git', :tag => s.version.to_s }
-  s.social_media_url = 'https://twitter.com/michalkonturek'
-  s.ios.deployment_target = '8.0'
+  s.source           = { :git => 'https://github.com/cj-zeiger/LatoFont.git', :tag => s.version.to_s }
+  s.ios.deployment_target = '10.0'
   s.source_files = 'LatoFont/Classes/**/*'
   s.resource_bundles = {
     'LatoFont' => ['LatoFont/Assets/*.ttf']
--- LatoFont/Classes/UIFont+Lato.swift
+++ LatoFont/Classes/UIFont+Lato.swift
@@ -1,4 +1,3 @@
-//
 //  UIFont+Lato.swift
 //  LatoFont
 //
@@ -13,130 +12,139 @@ import CoreText
  Fake class required for Font bundle loading.
  */
 class LatoFontFakeClass {
-    
+
 }
 
-extension UIFont {
+let fonts: [String] = [
+    "Lato-Hairline",
+    "Lato-HairlineItalic",
+    "Lato-Light",
+    "Lato-LightItalic",
+    "Lato-Regular",
+    "Lato-Italic",
+    "Lato-Bold",
+    "Lato-BoldItalic",
+    "Lato-Black",
+    "Lato-BlackItalic"
+]
 
-    @nonobjc static var fonts: [String]!
-    
-    override public class func initialize() {
-        fonts = [
-            "Lato-Hairline",
-            "Lato-HairlineItalic",
-            "Lato-Light",
-            "Lato-LightItalic",
-            "Lato-Regular",
-            "Lato-Italic",
-            "Lato-Bold",
-            "Lato-BoldItalic",
-            "Lato-Black",
-            "Lato-BlackItalic"
-        ]
-        
-        let podBundle = NSBundle(forClass: LatoFontFakeClass.self)
-        if let bundleURL = podBundle.URLForResource("LatoFont", withExtension: "bundle") {
-            if let bundle = NSBundle(URL: bundleURL) {
-                
-                for font in fonts {
-                    let url = bundle.URLForResource(font, withExtension: "ttf")
-                    
-                    if (url != nil) {
-                        var errorRef: Unmanaged<CFError>?
-                        CTFontManagerRegisterFontsForURL(url!, .None, &errorRef)
-                    }
+func fontInit()
+{
+    let podBundle = Bundle(for: LatoFontFakeClass.self)
+    if let bundleURL = podBundle.url(forResource: "LatoFont", withExtension: "bundle") {
+        if let bundle = Bundle(url: bundleURL) {
+
+            for font in fonts {
+                let url = bundle.url(forResource: font, withExtension: "ttf")
+
+                if (url != nil) {
+                    var errorRef: Unmanaged<CFError>?
+                    CTFontManagerRegisterFontsForURL(url! as CFURL, .none, &errorRef)
                 }
             }
         }
     }
-    
+}
+
+extension UIFont {
+
     /**
-     Returns `Lato` font with `hairline` style.
-     
+     Returns Lato font with hairline style.
+
      - parameter size: size of the font
      */
     public class func latoHairline(size: CGFloat) -> UIFont! {
+        fontInit()
         return UIFont.init(name: fonts[0], size: size);
     }
-    
+
     /**
-     Returns `Lato` font with `hairline italic` style.
-     
+     Returns Lato font with hairline italic style.
+
      - parameter size: size of the font
      */
     public class func latoHairlineItalic(size: CGFloat) -> UIFont! {
+        fontInit()
         return UIFont.init(name: fonts[1], size: size);
     }
-    
+
     /**
-     Returns `Lato` font with `light` style.
-     
+     Returns Lato font with light style.
+
      - parameter size: size of the font
      */
     public class func latoLight(size: CGFloat) -> UIFont! {
+        fontInit()
         return UIFont.init(name: fonts[2], size: size);
     }
-    
+
     /**
-     Returns `Lato` font with `light italic` style.
-     
+     Returns Lato font with light italic style.
+
      - parameter size: size of the font
      */
     public class func latoLightItalic(size: CGFloat) -> UIFont! {
+        fontInit()
         return UIFont.init(name: fonts[3], size: size);
     }
-    
+
     /**
-     Returns `Lato` font with `lato` style.
-     
+     Returns Lato font with lato style.
+
      - parameter size: size of the font
      */
     public class func lato(size: CGFloat) -> UIFont! {
+        fontInit()
         return UIFont.init(name: fonts[4], size: size);
     }
-    
+
     /**
-     Returns `Lato` font with `italic` style.
-     
+     Returns Lato font with italic style.
+
      - parameter size: size of the font
      */
     public class func latoItalic(size: CGFloat) -> UIFont! {
+        fontInit()
         return UIFont.init(name: fonts[5], size: size);
     }
-    
+
     /**
-     Returns `Lato` font with `bold` style.
-     
+     Returns Lato font with bold style.
+
      - parameter size: size of the font
      */
     public class func latoBold(size: CGFloat) -> UIFont! {
+        fontInit()
         return UIFont.init(name: fonts[6], size: size);
     }
-    
+
     /**
-     Returns `Lato` font with `bold italic` style.
-     
+     Returns Lato font with bold italic style.
+
      - parameter size: size of the font
      */
     public class func latoBoldItalic(size: CGFloat) -> UIFont! {
+        fontInit()
         return UIFont.init(name: fonts[7], size: size);
     }
-    
+
     /**
-     Returns `Lato` font with `black` style.
-     
+     Returns Lato font with black style.
+
      - parameter size: size of the font
      */
     public class func latoBlack(size: CGFloat) -> UIFont! {
+        fontInit()
         return UIFont.init(name: fonts[8], size: size);
     }
-    
+
     /**
-     Returns `Lato` font with `black italic` style.
-     
+     Returns Lato font with black italic style.
+
      - parameter size: size of the font
      */
     public class func latoBlackItalic(size: CGFloat) -> UIFont! {
+        fontInit()
         return UIFont.init(name: fonts[9], size: size);
     }
 
